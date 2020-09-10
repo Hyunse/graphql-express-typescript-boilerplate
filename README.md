b4d02ad7150972af79bc4824e7f90af4d30c94a6
Kim Hyunse / Arthur <anzmf12@naver.com>
2020-09-10 00:24:55 -0400

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Graphql Express Typescript Boilerplate
+
+- Graphql
+- Express
+- Typescript
+- Postgresql
+- TypeORM
+
+## Install
+<pre>
+  <code>
+    /* Install */
+    yarn
+    
+    /* Run */
+    yarn dev
+  </code>
+</pre>
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import dotenv from 'dotenv';
 import path from 'path';
 
-dotenv.config({ path: path.resolve(__dirname, './.env') });
+dotenv.config({ path: path.resolve(__dirname, './.env.development') });
+// dotenv.config({ path: path.resolve(__dirname, './.env') });
 
 import app from './app';
 import { Options } from 'graphql-yoga';
